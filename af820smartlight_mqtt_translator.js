b885f00fbff27aa97e3b846f253836ea780fd0d8
macmattes <mattes.b@gmx.net>
2016-12-15 18:34:58 +0100

    Update af820smartlight_mqtt_translator.js

--- af820smartlight_mqtt_translator.js
+++ af820smartlight_mqtt_translator.js
@@ -4,11 +4,9 @@ var Adapter = require('strong-pubsub-mqtt');
 var sub = new Client({host: '127.0.0.1', port: 1883}, Adapter);
 var pub = new Client({host: '127.0.0.1', port: 1883}, Adapter);
 
-var stopic = '/smartlight/EG_Wohnzimmer'
-
-sub.subscribe('stat_json' + stopic);
-
+sub.subscribe('stat_json/smartlight/#');
 sub.on('message', function(topic, msg) {
+ var subtopic = 'stat' + topic.substr(9, topic.length-9);
  console.log(topic, msg.toString());
 
  var data = JSON.parse(msg);
@@ -22,7 +20,7 @@ sub.on('message', function(topic, msg) {
     } else {
       result.push(value);
       console.log(prop,value);
-      sub.publish('stat' + stopic + prop, value.toString());
+      sub.publish( subtopic + prop, value.toString());
     }
   }
   return result;
@@ -32,31 +30,31 @@ sub.on('message', function(topic, msg) {
 
 })
 
-pub.subscribe('cmnd' + stopic + '/#');
-
+pub.subscribe('cmnd/smartlight/#');
 pub.on('message', function(topic, msg) {
  console.log(topic, msg.toString());
  var tres = topic.split("/");
  var res = msg.toString().split(",");
- console.log(res[0],tres.length,tres[tres.length-1]);
+ var pubtopic = 'cmnd_json' + topic.substr(4, topic.length-4);
+ 
+ console.log(pubtopic, res[0],tres.length);
 
    if (msg.toString() === 'on') {
-      pub.publish('cmnd_json' + stopic, '{ "cmd" : "turnOn" }');
+      pub.publish( pubtopic , '{ "cmd" : "turnOn" }');
    } else if (msg.toString() === 'off') {
-      pub.publish('cmnd_json' + stopic, '{ "cmd" : "turnOff" }');
+      pub.publish( pubtopic , '{ "cmd" : "turnOff" }');
    } else if (msg.toString() === 'checkStatus') {
-      pub.publish('cmnd_json' + stopic, '{ "cmd"  :  "checkStatus" }');
+      pub.publish( pubtopic , '{ "cmd"  :  "checkStatus" }');
    } else if (msg.toString() === 'checkWifi') {
-      pub.publish('cmnd_json' + stopic, '{ "cmd"  :  "checkWifi" }');
-   } else if (msg.toString() === 'sleep') {
-      pub.publish('cmnd_json' + stopic, '{ "cmd"  :  "sleep", "time" : ' + res[0] + ' }');
+      pub.publish( pubtopic , '{ "cmd"  :  "checkWifi" }');
    } else if (msg.toString() === 'register') {
-      pub.publish('cmnd_json' + stopic, '{ "cmd"  :  "register" }');
+      pub.publish( pubtopic , '{ "cmd"  :  "register" }');
+   } else if (tres[tres.length-1] === 'setSleep') {
+      pub.publish( pubtopic.substr(0, pubtopic.length - 9) , '{ "cmd"  :  "sleep", "time" : ' + res[0] + ' }');
    } else if (tres[tres.length-1] === 'setColorRGB') {
-      pub.publish('cmnd_json' + stopic, '{ "cmd" : "setColor", "red": ' + parseInt(res[0].substr(0, 2), 16) + ', "green": ' + parseInt(res[0].substr(2, 2), 16) + ', "blue": ' + parseInt(res[0].substr(4, 2), 16) + ', "white": 0, "time": 300, "lux": 0 }');
+      pub.publish( pubtopic.substr(0, pubtopic.length - 12) , '{ "cmd" : "setColor", "red": ' + parseInt(res[0].substr(0, 2), 16) + ', "green": ' + parseInt(res[0].substr(2, 2), 16) + ', "blue": ' + parseInt(res[0].substr(4, 2), 16) + ', "white": 0, "time": 300, "lux": 0 }');
    } else if (tres[tres.length-1] === 'setColorWhite') {
-      pub.publish('cmnd_json' + stopic, '{ "cmd" : "setColor", "red": 0, "green": 0, "blue": 0, "white": ' + res[0] + ', "time": 300, "lux": ' + res[0] + ' }');
+      pub.publish( pubtopic.substr(0, pubtopic.length - 14) , '{ "cmd" : "setColor", "red": 0, "green": 0, "blue": 0, "white": ' + res[0] + ', "time": 300, "lux": ' + res[0] + ' }');
    }
-
 
 });
